1ef9d56d313a64ed8c8fb6e82be49350f9881f42
dongsub-joung <joungdongsub1103@gmail.com>
2021-05-13 13:45:37 +0900

    Update Logic && Component

--- 11_Week/java/src/project2_submit_version/Calculator_Swing.java
+++ 11_Week/java/src/project2_submit_version/Calculator_Swing.java
@@ -1,88 +1,110 @@
 package project2_submit_version;
 
 import project2_ver0.HoleOption;
-import project2_ver0.Init;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Calculator_Swing extends JFrame {
 
     Container CP = getContentPane();
-    JLabel number1= new JLabel("Number1");
-    JLabel number2= new JLabel("Number2");
-    JLabel result= new JLabel("result");
+    JLabel number1 = new JLabel("Number1");
+    JLabel number2 = new JLabel("Number2");
+    JLabel result = new JLabel("result");
     JTextField numb1_field = new JTextField("", SwingConstants.RIGHT);
     JTextField numb2_field = new JTextField("", SwingConstants.RIGHT);
     JTextField result_field = new JTextField("", SwingConstants.RIGHT);
     JPanel panel = new JPanel();
 
-    Init init= new Init();
     static String view_value = "";
+    String[] buff = new String[5];
 
     /**
      * Event Method
      */
     private void evenHandlerOperator(JButton btn) {
         btn.addActionListener(e -> {
-            String temp, current_val= "";
+            String value1 = numb1_field.getText();
+            String value2 = numb2_field.getText();
+            double a = 0, b = 0;
+            String c = "";
+            double result = 0;
+            String view= "";
+            int buff_len = 0;
 
 //            Get Value entered by user && Null Checking.Verify
-            init.saveFirstValue(current_val);
-            init.saveOperateVal(btn.getText());
-            label.setText("");
-        });
-    }
+            buff_len = buff.length;
+            if (!value1.equals("")) {
+                a = Integer.parseInt(value1);
+                b = Integer.parseInt(value2);
+                c = btn.getText();
 
-    private void evenHandlerEqual(JButton btn) {
-        btn.addActionListener(e -> {
-            String temp, current_val= "";
-
-//            Get Value entered by user && Null Checking.Verify
-            temp= label.getText();
-            if(temp != null) {
-                if (!temp.equals("")){
-                    current_val= temp;
-                }else{
-                    System.out.println("Err: Default number");
-                }
-            }
-
-            if (init.buff[1] != null) {
-                if ()
-                {
-                    init.saveSecondValue(current_val);
-                    logic.cal();
-                    view_value = logic.getResult() + "";
-                    label.setText(view_value);
-                } else  {
-                    System.out.println("duplication value");
+                if (buff_len > 0) {
+                    switch (c) {
+                        case "+":
+                            result = a + b;
+                            break;
+                        case "-":
+                            result = a - b;
+                            break;
+                        case "*":
+                            result = a * b;
+                            break;
+                        case "/":
+                            result = a / b;
+                            break;
+                        default:
+                            System.out.println("ERR");
+                            break;
+                    }
+                    view= String.valueOf(result);
+                    result_field.setText(view);
+                } else {
+                    result_field.setText("Operator is null");
+                    return;
                 }
             } else {
-                System.out.println("Operator is null");
+                result_field.setText("First number is Null.");
                 return;
             }
         });
     }
 
     private void evenHandlerClear(JButton btn) {
-        btn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                init.resetAll();
-                label.setText("");
-            }
+        btn.addActionListener(e -> {
+            numb1_field.setText("");
+            numb2_field.setText("");
+            result_field.setText("");
         });
     }
+
 
     /**
      * Component Method
      */
+    private void addSingingComponent(JLabel label, JTextField field){
+        panel.add(label);
+        panel.add(field);
+    }
+
     private void addBtnToPanel(JButton... btns) {
         for (int i = 0; i < btns.length; i++) {
             panel.add(btns[i]);
+        }
+    }
+
+    private void addEventHandler(JButton... btns) {
+        for (JButton btn : btns) {
+            evenHandlerOperator(btn);
+        }
+    }
+
+    private void setFontJTextFields(JTextField... fields) {
+        for (JTextField field : fields) {
+            field.setFont(
+                    new Font(project2.HoleOption.VIEW_FONT
+                            , Font.BOLD
+                            , project2.HoleOption.VIEW_SIZE));
         }
     }
 
@@ -93,39 +115,38 @@ public class Calculator_Swing extends JFrame {
         setTitle(project2_ver0.HoleOption.TITLE);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-//        Set GridLayout
-        GridLayout layout = new GridLayout(project2_ver0.HoleOption.ROW, project2_ver0.HoleOption.COLUMN);
-
-//        Set JTextField
-        label.setFont(new Font(project2_ver0.HoleOption.VIEW_FONT, Font.BOLD, project2_ver0.HoleOption.VIEW_SIZE));
-        label.setEditable(false);
-        CP.add(label, BorderLayout.NORTH);
+//        Set Component Options
+        setFontJTextFields(numb1_field, numb2_field, result_field);
+        numb1_field.setEditable(true);
+        numb2_field.setEditable(true);
+        result_field.setEditable(false);
 
 //        Component: BTN NUMBERS (4*4): Create BTN
-//        Or JButton[] bt = new JButton[16];
-        JButton div = new JButton("/");
-        evenHandlerOperator(div);
-        JButton mul = new JButton("*");
-        evenHandlerOperator(mul);
-        JButton sub = new JButton("-");
-        evenHandlerOperator(sub);
         JButton add = new JButton("+");
-        evenHandlerOperator(add);
-        JButton AC = new JButton("C");
+        JButton sub = new JButton("-");
+        JButton div = new JButton("/");
+        JButton mul = new JButton("*");
+        addEventHandler(add, sub, div, mul);
+
+        JButton AC = new JButton("Clear");
         evenHandlerClear(AC);
-        JButton equal = new JButton("=");
-        evenHandlerEqual(equal);
+
+        JButton EXIT = new JButton("EXIT");
+        EXIT.addActionListener(e -> System.exit(0) );
 
         CP.add(panel);
-        panel.setLayout(layout);
+//        GridLayout layout = new GridLayout(project2_ver0.HoleOption.ROW, project2_ver0.HoleOption.COLUMN);
+//        panel.setLayout(layout);
 
-        addBtnToPanel(add, sub,mul, div);;
+//        Attach components
+        addSingingComponent( number1, numb1_field);
+        addSingingComponent( number2, numb2_field);
+        addSingingComponent( result, result_field);
+        addBtnToPanel(add, sub, mul, div);
+        addBtnToPanel(AC, EXIT);
         CP.add(panel, BorderLayout.CENTER);
 
-        TA.setEditable(false);
-        CP.add(TA, BorderLayout.SOUTH);
-
-        CP.setLayout(layout);
+//        CP.setLayout(layout);
         setSize(project2_ver0.HoleOption.W, HoleOption.H);
         setVisible(true);
     }
---
+++ 11_Week/java/src/project2_ver0/Calculator_Swing.java
@@ -0,0 +1,144 @@
+//package project2_ver0;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//public class Calculator_Swing extends JFrame {
+//
+//    Container CP = getContentPane();
+//    JLabel number1= new JLabel("Number1");
+//    JLabel number2= new JLabel("Number2");
+//    JLabel result= new JLabel("result");
+//    JTextField numb1_field = new JTextField("", SwingConstants.RIGHT);
+//    JTextField numb2_field = new JTextField("", SwingConstants.RIGHT);
+//    JTextField result_field = new JTextField("", SwingConstants.RIGHT);
+//    JPanel panel = new JPanel();
+//
+//    Init init= new Init();
+//    static String view_value = "";
+//
+//    /**
+//     * Event Method
+//     */
+//    private void evenHandlerOperator(JButton btn) {
+//        btn.addActionListener(e -> {
+//            String temp, current_val= "";
+//
+////            Get Value entered by user && Null Checking.Verify
+//            init.saveFirstValue(current_val);
+//            init.saveOperateVal(btn.getText());
+//            label.setText("");
+//        });
+//    }
+//
+//    private void evenHandlerEqual(JButton btn) {
+//        btn.addActionListener(e -> {
+//            String temp, current_val= "";
+//
+////            Get Value entered by user && Null Checking.Verify
+//            temp= label.getText();
+//            if(temp != null) {
+//                if (!temp.equals("")){
+//                    current_val= temp;
+//                }else{
+//                    System.out.println("Err: Default number");
+//                }
+//            }
+//
+//            if (init.buff[1] != null) {
+//                if ()
+//                {
+//                    init.saveSecondValue(current_val);
+//                    logic.cal();
+//                    view_value = logic.getResult() + "";
+//                    label.setText(view_value);
+//                } else  {
+//                    System.out.println("duplication value");
+//                }
+//            } else {
+//                System.out.println("Operator is null");
+//                return;
+//            }
+//        });
+//    }
+//
+//    private void evenHandlerClear(JButton btn) {
+//        btn.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                init.resetAll();
+//                label.setText("");
+//            }
+//        });
+//    }
+//
+//    /**
+//     * Component Method
+//     */
+//    private void addBtnToPanel(JButton... btns) {
+//        for (int i = 0; i < btns.length; i++) {
+//            panel.add(btns[i]);
+//        }
+//    }
+//
+//    /**
+//     * Constructor: Init
+//     */
+//    private Calculator_Swing() {
+//        setTitle(project2_ver0.HoleOption.TITLE);
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+//
+////        Set GridLayout
+//        GridLayout layout = new GridLayout(project2_ver0.HoleOption.ROW, project2_ver0.HoleOption.COLUMN);
+//
+////        Set JTextField
+//        label.setFont(new Font(project2_ver0.HoleOption.VIEW_FONT, Font.BOLD, project2_ver0.HoleOption.VIEW_SIZE));
+//        label.setEditable(false);
+//        CP.add(label, BorderLayout.NORTH);
+//
+////        Component: BTN NUMBERS (4*4): Create BTN
+////        Or JButton[] bt = new JButton[16];
+//        JButton div = new JButton("/");
+//        evenHandlerOperator(div);
+//        JButton mul = new JButton("*");
+//        evenHandlerOperator(mul);
+//        JButton sub = new JButton("-");
+//        evenHandlerOperator(sub);
+//        JButton add = new JButton("+");
+//        evenHandlerOperator(add);
+//        JButton AC = new JButton("C");
+//        evenHandlerClear(AC);
+//        JButton equal = new JButton("=");
+//        evenHandlerEqual(equal);
+//
+//        CP.add(panel);
+//        panel.setLayout(layout);
+//
+//        addBtnToPanel(add, sub,mul, div);;
+//        CP.add(panel, BorderLayout.CENTER);
+//
+//        TA.setEditable(false);
+//        CP.add(TA, BorderLayout.SOUTH);
+//
+//        CP.setLayout(layout);
+//        setSize(project2_ver0.HoleOption.W, HoleOption.H);
+//        setVisible(true);
+//    }
+//
+//    private static class InnerInstanceClass {
+//        private static final Calculator_Swing instance = new Calculator_Swing();
+//    }
+//
+//    public static Calculator_Swing getInstance() {
+//        return InnerInstanceClass.instance;
+//    }
+//
+//    public static void main(String[] args) {
+//        getInstance();
+//    }
+//}
+//
+//// 디자인 참고할만한 사이트
+//// https://m.blog.naver.com/dawonchu99/221754223779
