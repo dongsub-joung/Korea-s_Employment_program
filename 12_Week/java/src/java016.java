76387bae82e774a322a64a3d0edabb8621f0eae6
dongsub-joung <joungdongsub1103@gmail.com>
2021-05-20 23:20:54 +0900

    Update

--- 12_Week/java/src/java016.java
+++ 12_Week/java/src/java016.java
@@ -4,31 +4,49 @@ import java.awt.*;
 class Info{
     final static String TITLE= "txt";
     final static String[] MENUS= {"Next", "Previous", "First", "Last"};
+    final static int W= 350, H= 250;
 }
 
 public class java016 extends JFrame {
     Container container;
-    JPanel jPanel;
     CardLayout cardLayout;
-    JButton next, previous, first, last;
+    GridLayout gridLayout;
 
-//    Create Components
-    final int NEXT=0, PREVIOUS=1, FIRST=2, LAST=3;
-    private void addBtns(JButton ...buttons){
-        for (int i = 0; i < Info.MENUS.length; i++) {
-            buttons[i]= new JButton(Info.MENUS[i]);
-        }
-    }
+//    final int NEXT=0, PREVIOUS=1, FIRST=2, LAST=3;
+    final int MENUS_SIZE= Info.MENUS.length;
+    JPanel[] panels= new JPanel[MENUS_SIZE];
+    JButton[] buttons= new JButton[MENUS_SIZE];
+    JLabel[] labels= new JLabel[MENUS_SIZE];
 
     java016(){
         container= getContentPane();
-        jPanel= new JPanel();
         cardLayout= new CardLayout();
+        gridLayout= new GridLayout(4,1);
 
+//        Setting Head
         setTitle(Info.TITLE);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(Info.W, Info.H);
+        setVisible(true);
 
-        addBtns(next, previous, first, last);
+//        Setting Body
+        //    Create Components
+        for (int i = 0; i < MENUS_SIZE; i++) {
+            buttons[i]= new JButton(Info.MENUS[i]);
+            panels[i]= new JPanel();
+            labels[i]= new JLabel(Info.MENUS[i]);
+        }
+        for (JButton button:buttons){
+            add(button); // container
+        }
+
+        //    Add Component at panel
+        final String DEFAULT_STR= "Panel"; int count= 1;
+        for (JPanel panel:panels){
+            String count_str= ((count++) +"");
+            panel.add(new JLabel(DEFAULT_STR + count_str));
+            container.add(panel);
+        }
     }
 
     public static void main(String[] args) {
