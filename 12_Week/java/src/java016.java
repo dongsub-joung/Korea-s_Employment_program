0b46998c7546c572b2aca7875eeb165fadb8a72e
dongsub-joung <joungdongsub1103@gmail.com>
2021-05-21 04:04:13 +0900

    Update

--- 12_Week/java/src/java016.java
+++ 12_Week/java/src/java016.java
@@ -7,21 +7,56 @@ class Info{
     final static int W= 350, H= 250;
 }
 
+//    final int NEXT=0, PREVIOUS=1, FIRST=2, LAST=3;
+class Panel_L extends JPanel{
+    int MENUS_SIZE = Info.MENUS.length;
+    public JButton[] buttons;
+    {
+        for (int i = 0; i < MENUS_SIZE; i++) {
+            buttons = new JButton[MENUS_SIZE];
+            buttons[i]= new JButton(Info.MENUS[i]);
+            add(buttons[i]);
+        }
+    }
+    public Panel_L(){
+//        EventHandler
+    }
+}
+
+class Panel_R extends JPanel{
+    int MENUS_SIZE = Info.MENUS.length;
+    public JPanel[] panels;
+
+    {
+        panels = new JPanel[MENUS_SIZE];
+        for (int i = 0; i < MENUS_SIZE; i++) {
+            panels[i] = new JPanel();
+        }
+    }
+    public Panel_R(){
+        final String DEFAULT_STR;
+        int count;
+
+        // Add Component at panel
+        DEFAULT_STR= "Panel"; count= 1;
+        for (JPanel panel: panels){
+            String count_str= (count++) + "";
+            panel.add(new JLabel(DEFAULT_STR + count_str));
+        }
+    }
+}
+
 public class java016 extends JFrame {
     Container container;
-    CardLayout cardLayout;
-    GridLayout gridLayout;
-
-//    final int NEXT=0, PREVIOUS=1, FIRST=2, LAST=3;
-    final int MENUS_SIZE= Info.MENUS.length;
-    JPanel[] panels= new JPanel[MENUS_SIZE];
-    JButton[] buttons= new JButton[MENUS_SIZE];
-    JLabel[] labels= new JLabel[MENUS_SIZE];
+    JPanel panel_L= null;
+    JPanel panel_R= null;
 
     java016(){
         container= getContentPane();
-        cardLayout= new CardLayout();
-        gridLayout= new GridLayout(4,1);
+        JPanel panel_L= new Panel_L();
+        JPanel panel_R= new Panel_R();
+        panel_L.setLayout(new GridLayout(4,1));
+        panel_R.setLayout(new CardLayout());
 
 //        Setting Head
         setTitle(Info.TITLE);
@@ -30,23 +65,8 @@ public class java016 extends JFrame {
         setVisible(true);
 
 //        Setting Body
-        //    Create Components
-        for (int i = 0; i < MENUS_SIZE; i++) {
-            buttons[i]= new JButton(Info.MENUS[i]);
-            panels[i]= new JPanel();
-            labels[i]= new JLabel(Info.MENUS[i]);
-        }
-        for (JButton button:buttons){
-            add(button); // container
-        }
-
-        //    Add Component at panel
-        final String DEFAULT_STR= "Panel"; int count= 1;
-        for (JPanel panel:panels){
-            String count_str= ((count++) +"");
-            panel.add(new JLabel(DEFAULT_STR + count_str));
-            container.add(panel);
-        }
+        container.add(panel_L);
+        container.add(panel_R);
     }
 
     public static void main(String[] args) {
