65c288d5b6edffd721b921caa02bfd81b568db3f
dongsub-joung <joungdongsub1103@gmail.com>
2021-05-22 02:05:41 +0900

    19-02

--- 12_Week/java/src/java19.java
+++ 12_Week/java/src/java19.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
 public class java19 {
+    final String PATH= System.getProperty("user.dir") + "/12_Week/DATA";
+    final String INPUT= PATH + "/text1.txt";
+
     public void ex01(){
-        final String PATH= System.getProperty("user.dir") + "/12_Week/DATA";
-        final String INPUT= PATH + "/text1.txt";
         final String OUTPUT= PATH + "/output1.txt";
 
-//        fileOutputStream= new FileOutputStream(OUTPUT)
         try(FileInputStream fileInputStream= new FileInputStream(new File(INPUT));
             FileOutputStream fileOutputStream= new FileOutputStream(new File(OUTPUT));) {
             fileInputStream.transferTo(fileOutputStream);
@@ -16,7 +16,19 @@ public class java19 {
         }
     }
 
+    public void ex02(){
+        final String OUTPUT= PATH + "/output2.txt";
+
+        try (BufferedInputStream bufferedInputStream= new BufferedInputStream(new FileInputStream(new File(INPUT)));
+            BufferedOutputStream bufferedOutputStream= new BufferedOutputStream(new FileOutputStream(new File(OUTPUT)))) {
+            bufferedInputStream.transferTo(bufferedOutputStream);
+        } catch (Exception e) {
+            System.out.println(e + "err");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        new java19().ex01();
+        new java19().ex02();
     }
 }
