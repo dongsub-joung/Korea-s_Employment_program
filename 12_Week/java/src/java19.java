6a36ea9ca13394931e898f9971abbee922d74cd7
dongsub-joung <joungdongsub1103@gmail.com>
2021-05-23 23:36:35 +0900

    Update java example19

--- 12_Week/java/src/java19.java
+++ 12_Week/java/src/java19.java
@@ -1,8 +1,10 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class java19 {
     final String PATH= System.getProperty("user.dir") + "/12_Week/DATA";
     final String INPUT= PATH + "/text1.txt";
+    // transferTo buffer size= 8192
 
     public void ex01(){
         final String OUTPUT= PATH + "/output1.txt";
@@ -28,7 +30,44 @@ public class java19 {
         }
     }
 
+    public void ex03(){
+        final String OUTPUT= PATH + "/output3.txt";
+
+        try(FileReader fileReader= new FileReader(new File(INPUT));
+            FileWriter fileWriter= new FileWriter(new File(OUTPUT))) {
+            fileReader.transferTo(fileWriter);
+        } catch (Exception e) {
+            System.out.println(e + "err");
+            e.printStackTrace();
+        }
+    }
+
+    //        BufferedInputStream: byte,array size= readUnsignedShort * 2
+    public void ex04(){
+        final String INPUTS= PATH + "/word.txt";
+        ArrayList<String> strs= new ArrayList<>();
+
+        try(FileReader fileReader= new FileReader(new File(INPUTS));
+            BufferedReader bufferedReader= new BufferedReader(fileReader)) {
+            String line= "";
+            while ((line= bufferedReader.readLine()) != null){
+                strs.add(line);
+            }
+            final int MAX= strs.size();
+            int selected_index= (int) (Math.random()*MAX);
+
+            System.out.println(strs.get(selected_index));
+        } catch (Exception e) {
+            System.out.println(e + "err");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        new java19().ex02();
+        java19 j19= new java19();
+//        j19.ex01();
+//        j19.ex02();
+//        j19.ex03();
+        j19.ex04();
     }
 }
